af0b8bfec2116406b2907ff8eec362da24ddf7d2
jisu <ahteostnqls@naver.com>
2024-03-11 17:00:01 +0900

    [ADD] Nav 컴포넌트 작업

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <Link href="/">HOME</Link>
+      <Link href="/ssgRendering">SSG</Link>
+      <Link href="/isrRendering">ISR</Link>
+      <Link href="/csrRendering">CSR</Link>
+      <Link href="/ssrRendering">SSR</Link>
+    </nav>
+  );
+};
