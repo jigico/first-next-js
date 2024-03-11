1338efb6acfd119e0c88e22b08c814ed6cacd1a7
jisu <ahteostnqls@naver.com>
2024-03-11 20:32:17 +0900

    [MOD] Nav 컴포넌트 ui 스타일링 작업

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 export const Nav = () => {
   return (
-    <nav>
-      <Link href="/">HOME</Link>
-      <Link href="/ssgRendering">SSG</Link>
-      <Link href="/isrRendering">ISR</Link>
-      <Link href="/csrRendering">CSR</Link>
-      <Link href="/ssrRendering">SSR</Link>
-    </nav>
+    <header>
+      <nav className="p-4 flex items-center justify-center gap-1.5 border-b">
+        <Link href="/">NEXT JS 공부</Link>
+        <Link href="/ssgRendering">SSG</Link>
+        <Link href="/isrRendering">ISR</Link>
+        <Link href="/csrRendering">CSR</Link>
+        <Link href="/ssrRendering">SSR</Link>
+      </nav>
+    </header>
   );
 };
